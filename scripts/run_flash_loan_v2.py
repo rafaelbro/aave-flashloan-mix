be7f576164b330099d5d264d4c0514319472cd03
Rafael <rafael.heck@outlook.com>
2021-06-02 16:50:25 -0300

    1st changes

--- scripts/run_flash_loan_v2.py
+++ scripts/run_flash_loan_v2.py
@@ -1,6 +1,6 @@
 from brownie import FlashloanV2, accounts, config, network, interface
 
-MINIMUM_FLASHLOAN_WETH_BALANCE = 500000000000000000
+MINIMUM_FLASHLOAN_WETH_BALANCE = 500000000000000000 #0.5WETH
 ETHERSCAN_TX_URL = "https://kovan.etherscan.io/tx/{}"
 
 
@@ -13,6 +13,7 @@ def main():
     flashloan = FlashloanV2[len(FlashloanV2) - 1]
     weth = interface.WethInterface(config["networks"][network.show_active()]["weth"])
     # We need to fund it if it doesn't have any token to fund!
+    print("current balance of contract: ", weth.balanceOf(flashloan))
     if weth.balanceOf(flashloan) < MINIMUM_FLASHLOAN_WETH_BALANCE:
         print("Funding Flashloan contract with WETH...")
         weth.transfer(flashloan, "1 ether", {"from": acct})
